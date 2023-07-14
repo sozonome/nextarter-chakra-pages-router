0af6de522268841b07b2db941e1f5e40ed8709f9
Agustinus Nathaniel <berserk.nate@gmail.com>
2023-07-14 12:55:46 +0700

    Initial commit

---
+++ src/lib/components/samples/CTASection.tsx
@@ -0,0 +1,64 @@
+import { Box, Button, Flex, Image, Link } from '@chakra-ui/react';
+import { AiFillGithub } from 'react-icons/ai';
+
+const repoLink = 'https://github.com/sozonome/nextarter-chakra';
+
+const CTASection = () => {
+  return (
+    <Box textAlign="center">
+      <Box transform="scale(0.85)">
+        <Flex marginY={4} justifyContent="center" gap={2}>
+          <Link
+            aria-label="Deploy to Vercel"
+            isExternal
+            rel="noopener noreferrer"
+            href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fsozonome%2Fnextarter-chakra"
+          >
+            <Image
+              src="https://vercel.com/button"
+              alt="Vercel deploy button"
+              width="92px"
+              height="32px"
+            />
+          </Link>
+
+          <Link
+            aria-label="Deploy to Netlify"
+            isExternal
+            rel="noopener noreferrer"
+            href="https://app.netlify.com/start/deploy?repository=https://github.com/sozonome/nextarter-chakra"
+          >
+            <Image
+              src="https://www.netlify.com/img/deploy/button.svg"
+              alt="Netlify deploy button"
+              height="32px"
+              width="146px"
+            />
+          </Link>
+        </Flex>
+      </Box>
+
+      <Flex justifyContent="center" alignItems="center" gap={2}>
+        <Button
+          as="a"
+          href="https://github.com/sozonome/nextarter-chakra/generate"
+          target="_blank"
+          size="sm"
+        >
+          Use This Template
+        </Button>
+        <Button
+          as="a"
+          href={repoLink}
+          target="_blank"
+          leftIcon={<AiFillGithub />}
+          size="sm"
+        >
+          Open in Github
+        </Button>
+      </Flex>
+    </Box>
+  );
+};
+
+export default CTASection;
---
+++ src/lib/pages/home/index.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { Flex } from '@chakra-ui/react';
+
+import CTASection from '~/lib/components/samples/CTASection';
+import SomeImage from '~/lib/components/samples/SomeImage';
+import SomeText from '~/lib/components/samples/SomeText';
+
+const Home = () => {
+  return (
+    <Flex
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      minHeight="70vh"
+      gap={4}
+      mb={8}
+      w="full"
+    >
+      <SomeText />
+      <SomeImage />
+      <CTASection />
+    </Flex>
+  );
+};
+
+export default Home;
